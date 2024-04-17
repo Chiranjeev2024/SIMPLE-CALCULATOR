cfb573719f7654fdc73ad02695b3bcb9d8b52ac5
Chiranjeev2024 <sks7369064258@gmail.com>
2024-04-17 19:11:44 +0530

    instruction

--- SIMPLE-CALCULATOR.cpp
+++ SIMPLE-CALCULATOR.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main(){
 
-    cout<<"\t\t\t* SIMPLE CALCULATOR *"<<endl<<endl;
+    cout<<endl<<"\t\t\t* SIMPLE CALCULATOR *"<<endl<<endl;
 
+    cout<<"\t# NOTE:-"<<endl;
+    cout<<"\t   - you can choose any two number only."<<endl;
+    cout<<"\t   - you can choose only one operator, such as"<<endl;
+    cout<<"\t\t. Addition(+)"<<endl;
+    cout<<"\t\t. subtraction(-)"<<endl;
+    cout<<"\t\t. multiplication(/)"<<endl;
+    cout<<"\t\t. division(*)"<<endl;
+    cout<<"\t\t. Modulus(%)"<<endl;
+
+
+    return 0;
 }
